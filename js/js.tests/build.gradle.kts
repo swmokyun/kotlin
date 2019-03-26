8a0b23146e6e0dbeff79fe92a08de09c3fc8b7fc
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2019-03-26 14:46:46 +0300

    ~~~ disable parallel tests

--- js/js.tests/build.gradle.kts
+++ js/js.tests/build.gradle.kts
@@ -2,9 +2,9 @@ import com.moowork.gradle.node.NodeExtension
 import com.moowork.gradle.node.npm.NpmTask
 import org.gradle.internal.os.OperatingSystem
 
-tasks.withType<Test> {
-    maxParallelForks = Math.max(Runtime.getRuntime().availableProcessors() / 2, 1)
-}
+//tasks.withType<Test> {
+//    maxParallelForks = Math.max(Runtime.getRuntime().availableProcessors() / 2, 1)
+//}
 
 plugins {
     kotlin("jvm")
