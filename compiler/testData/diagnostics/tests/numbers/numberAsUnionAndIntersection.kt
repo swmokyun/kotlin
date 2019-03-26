d8b0941fceed46b13cf1e3a63a304171ef82f2ad
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2019-03-26 11:46:33 +0300

    [NI] Discriminate integer literal types as they are less specific

--- compiler/testData/diagnostics/tests/numbers/numberAsUnionAndIntersection.kt
+++ compiler/testData/diagnostics/tests/numbers/numberAsUnionAndIntersection.kt
@@ -2,9 +2,11 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 
 fun test(numbers: List<Number>) {
-    shortResult { getShort() ?: 1 } // Required Int, found Short
+    shortComparable { getShort() ?: 1 }
+    shortExpected { getShort() ?: 1 }
 }
 
-fun <R : Comparable<R>> shortResult(f: () -> R) {}
+fun <R> shortExpected(f: () -> R) {}
+fun <R : Comparable<R>> shortComparable(f: () -> R) {}
 
 fun getShort(): Short? = 1
